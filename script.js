6dad8d5922da359e9913b4984e8ff1509b5925b7
Tom Krok <36570258-trak2025zz@users.noreply.replit.com>
2024-09-18 15:52:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+(function () {
+  const example = document.getElementById('example')
+  const cw1 = document.getElementById('cw1')
+  const cw2 = document.getElementById('cw2')
+  const cw3 = document.getElementById('cw3')
+  const answer = document.getElementById('answer')
+
+  example.addEventListener("click", function () {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        console.log(array)
+        answer.innerHTML = JSON.stringify(array);
+      })
+  })
+
+  cw1.addEventListener("click", function () {
+    //TODO
+  })
+
+  cw2.addEventListener("click", function () {
+    //TODO
+  })
+
+  cw3.addEventListener("click", function () {
+    //TODO
+  })
+
+})();
