50b60c2f6b2f5d4f1f23356ebbc37bf733d98078
dawid <dawid566123@gmail.com>
2025-10-21 09:10:43 +0200

    2_1.1.

--- script.js
+++ script.js
@@ -15,7 +15,18 @@
   })
 
   cw1.addEventListener("click", function () {
-    //TODO
+    const list = document.createElement('ul');
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        console.log(array)
+        array.map((post) => {
+          const listElement = document.createElement('li');
+          listElement.innerText = `${post.id} - ${post.title}\n${post.body}\n\n`
+          list.appendChild(listElement);
+        })
+      })
+    answer.appendChild(list);
   })
 
   cw2.addEventListener("click", function () {
